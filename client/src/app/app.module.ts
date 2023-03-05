5c17c531a9557f733aba0423c668a9503ca9900f
razzouko <orazzouk8@boscdelacoma.cat>
2023-03-05 16:17:21 +0100

    commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JocComponent } from './projecte/components/joc/joc.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
