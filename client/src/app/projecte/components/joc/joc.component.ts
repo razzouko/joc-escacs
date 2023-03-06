b52380bd3d86bcf2e166ca512d4ac305fa5e7807
razzouko <100167727+razzouko@users.noreply.github.com>
2023-03-06 19:59:07 +0100

    drag and drop funcionant

--- client/src/app/projecte/components/joc/joc.component.ts
+++ client/src/app/projecte/components/joc/joc.component.ts
@@ -21,12 +21,62 @@ export class JocComponent implements OnInit {
   ngOnInit(): void {
     this.taula1 = new Taula(true);
     this.taula2 = new Taula(false);
-    
+    this.ruta = this.baseHref + "assets/imatges/";
   
   }
 
-  drop(event: CdkDragDrop<Fila>) {
-    console.log(event)
+  dropTaulell1(event: CdkDragDrop<any>) {
+    
+    let casellaOrigen = event.previousContainer.data;
+    let casellaDesti = event.container.data;
+
+    if(casellaDesti.informativa == true){
+      return;
+    }
+
+    if(casellaDesti.figura == ''){
+        let dadesOrigen = { fila : casellaOrigen.fila , columna : casellaOrigen.columna }
+        let dadesDesti = { fila : casellaDesti.fila , columna : casellaDesti.columna }
+        let mateixaFila = (casellaOrigen.fila == casellaDesti.fila) ? true: false;
+        this.moureFigura( "taulell1", casellaOrigen.figura , dadesOrigen , dadesDesti , mateixaFila);
+    }
+
   }
 
+
+  moureFigura( taulell : string , figura : string , origen : any , desti : any , mateixaFila : boolean){
+
+      if(taulell == "taulell1"){
+        let files = this.taula1.getFiles();
+        files.forEach(fila =>{
+          // si troba una fila relacionada amb l'origen o el desti
+            if(fila.numeroFila == parseInt(origen.fila) || fila.numeroFila == parseInt(desti.fila) ){
+              let caselles = fila.getCaselles();
+              caselles.forEach(casella => {
+
+                  if(mateixaFila){
+                    if(casella.columna == origen.columna){
+                      casella.figura = '';
+                    }
+
+                    if(casella.columna == desti.columna){
+                      casella.figura = figura;
+                    }
+                  }else{
+
+                    if(casella.columna == origen.columna && casella.fila == origen.fila){
+                      casella.figura = '';
+                    }
+
+                    if(casella.columna == desti.columna && casella.fila == desti.fila){
+                      casella.figura = figura;
+                    }
+                  }
+              });
+          
+            }
+        })
+      }
+
+  }
 }
